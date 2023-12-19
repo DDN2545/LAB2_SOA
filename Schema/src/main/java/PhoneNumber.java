002de9170fca24040a58747e89f535abd90b8733
phakhin.s <108508731+DDN2545@users.noreply.github.com>
2023-12-19 23:35:38 +0700

    Add files via upload

---
+++ Schema/src/main/java/PhoneNumber.java
@@ -0,0 +1,17 @@
+
+public class PhoneNumber {
+	private String number;
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public PhoneNumber(String string) {
+		// TODO Auto-generated constructor stub
+		this.number = string;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+}
